cc11620ecc5475c5c46968c463a18faddf15da6e
Jelle De Vleeschouwer <jelle.devleeschouwer.1@gmail.com>
2015-06-17 17:07:14 +0200

    [spacecatthree] - First commit

---
+++ spacecatthree/js/script.js
@@ -0,0 +1,28 @@
+var isLarge, isMedium, isSmall;
+
+isSmall = function() {
+  return matchMedia(Foundation.media_queries['small']).matches && !matchMedia(Foundation.media_queries.medium).matches;
+};
+
+isMedium = function() {
+  return matchMedia(Foundation.media_queries['medium']).matches && !matchMedia(Foundation.media_queries.large).matches;
+};
+
+isLarge = function() {
+  return matchMedia(Foundation.media_queries['large']).matches;
+};
+
+
+window.onresize = function(event) {
+	if (isSmall()) {
+		var x = document.getElementsByClassName("menu-item");
+		for (var i = 0; i < x.length; i++) {
+			x[i].style.height = "33.33%";
+		}
+	} else {
+		var x = document.getElementsByClassName("menu-item");
+		for (var i = 0; i < x.length; i++) {
+			x[i].style.height = "100%";
+		}
+	}
+};
